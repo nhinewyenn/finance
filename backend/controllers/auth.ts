27ada3e40069ea36cad8003bd5fb7de978366603
Nhi Nguyen <Nhi_nguyen1610@hotmail.com>
2024-01-22 16:08:52 +1100

    refactor

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -5,25 +5,20 @@ import bcrypt from 'bcrypt';
 
 export async function registerUser(req: Request, res: Response, next) {
   const { username, password } = req.body;
-  console.log(username, password);
 
   try {
-    bcrypt
-      .hash(password, 10)
-      .then(
-        async (hash) => await UserSchema.create({ username, password: hash })
-      )
-      .then((user) =>
-        res.status(200).json({ message: 'User successfully created', user })
-      );
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await UserSchema.create({
+      username,
+      password: hashedPassword,
+    });
+    res.status(200).json({ message: 'User successfully created', user });
   } catch (error) {
     res.status(401).json({ message: 'User not created' });
   }
-
-  next();
 }
 
-export async function login(req: Request, res: Response, next) {
+export async function login(req: Request, res: Response) {
   const { username, password } = req.body;
 
   if (!username || !password) {
@@ -34,30 +29,23 @@ export async function login(req: Request, res: Response, next) {
 
   try {
     const user = await UserSchema.findOne({ username });
-
     if (!user) {
-      res
+      return res
         .status(400)
         .json({ message: 'Login unsuccessful', error: 'User not found' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      res.status(200).json({ message: 'Login successful', user });
     } else {
-      // compare given password with hashed password
-      bcrypt.compare(password, user.password).then((result) => {
-        if (result) {
-          res.status(200).json({
-            message: 'Login successful',
-            user,
-          });
-        } else {
-          res.status(400).json({
-            message: 'Login unsuccessful',
-            error: 'Password do not match',
-          });
-        }
+      res.status(400).json({
+        message: 'Login unsuccessful',
+        error: 'Password do not match',
       });
     }
   } catch (error) {
-    res.status(400).json({
-      message: 'An error occurred',
-    });
+    res.status(400).json({ message: 'An error occurred with login process' });
   }
 }
