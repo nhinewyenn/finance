53187eb2895fc4ac31875dcdd5f4ad022e2b0976
Nhi Nguyen <Nhi_nguyen1610@hotmail.com>
2024-02-28 22:18:56 +1100

    rebuild

--- backend/dist/api/index.js
+++ backend/dist/api/index.js
@@ -1,42 +1,60 @@
-"use strict";
 /** @format */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+
+'use strict';
+/** @format */
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
+Object.defineProperty(exports, '__esModule', { value: true });
+const dotenv_1 = __importDefault(require('dotenv'));
 dotenv_1.default.config({ path: './.env' });
-const express_1 = __importDefault(require("express"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const cors_1 = __importDefault(require("cors"));
-const db_1 = require("../config/db");
-const errorMiddleware_1 = require("./middlewares/errorMiddleware");
-const transactionRoutes_1 = __importDefault(require("./transactionRoutes"));
-const userRoutes_1 = __importDefault(require("./userRoutes"));
-const authMiddleware_1 = require("./middlewares/authMiddleware");
-const path_1 = __importDefault(require("path"));
+const express_1 = __importDefault(require('express'));
+const cookie_parser_1 = __importDefault(require('cookie-parser'));
+const cors_1 = __importDefault(require('cors'));
+const db_1 = require('../config/db');
+const errorMiddleware_1 = require('./middlewares/errorMiddleware');
+const transactionRoutes_1 = __importDefault(require('./transactionRoutes'));
+const userRoutes_1 = __importDefault(require('./userRoutes'));
+const authMiddleware_1 = require('./middlewares/authMiddleware');
+const path_1 = __importDefault(require('path'));
 const app = (0, express_1.default)();
 const { PORT } = (_a = process.env) !== null && _a !== void 0 ? _a : 8000;
 // Middlewares
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({
+app.use(
+  (0, cors_1.default)({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-}));
+  })
+);
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../../frontend/dist'))); //serve the frontend static asset
+app.use(
+  express_1.default.static(
+    path_1.default.join(__dirname, '../../../frontend/dist')
+  )
+); //serve the frontend static asset
+// Middleware: Redirect root URL to login route
+app.get('/', (req, res) => {
+  res.redirect('/api/v1/auth/login');
+});
 // Routes
-app.use('/api/v1/profile', authMiddleware_1.verifyToken, transactionRoutes_1.default);
+app.use(
+  '/api/v1/profile',
+  authMiddleware_1.verifyToken,
+  transactionRoutes_1.default
+);
 app.use('/api/v1/auth', userRoutes_1.default);
-// Middleware
+// Middleware;
 app.use(errorMiddleware_1.notFound);
 app.use(errorMiddleware_1.errorHandler);
 function server() {
-    (0, db_1.db)();
-    app.listen(PORT, () => {
-        console.log('You are listening to PORT', PORT);
-    });
+  (0, db_1.db)();
+  app.listen(PORT, () => {
+    console.log('You are listening to PORT', PORT);
+  });
 }
 server();
--- backend/dist/api/middlewares/errorMiddleware.js
+++ backend/dist/api/middlewares/errorMiddleware.js
@@ -3,22 +3,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.errorHandler = exports.notFound = void 0;
 function notFound(req, res, next) {
-    const err = new Error(`Not found ${req.originalUrl}`);
-    res.status(404);
-    next(err);
+    const err = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404).json({
+        success: false,
+        message: err.message,
+    });
 }
 exports.notFound = notFound;
 function errorHandler(err, req, res, next) {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let status = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
     if (err.name === 'CastError' && err.kind === 'ObjectId') {
-        statusCode = 404;
+        status = 404;
         message = 'Resource not found';
     }
-    res.status(statusCode).json({
+    res.status(status).json({
         success: false,
-        message: message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        message,
     });
 }
 exports.errorHandler = errorHandler;
