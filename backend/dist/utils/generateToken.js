a3c9dec9d88e3e9b40aa1389159e0dcc3b04c5a3
Nhi Nguyen <Nhi_nguyen1610@hotmail.com>
2024-03-03 23:24:43 +1100

    ryn build

--- backend/dist/utils/generateToken.js
+++ backend/dist/utils/generateToken.js
@@ -1,21 +1,26 @@
-"use strict";
 /** @format */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+
+'use strict';
+/** @format */
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.generateToken = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const jsonwebtoken_1 = __importDefault(require('jsonwebtoken'));
 function generateToken(res, _id) {
-    const token = jsonwebtoken_1.default.sign({ _id }, process.env.SECRET_KEY, {
-        expiresIn: '14d',
-    });
-    res.cookie('access_token', token, {
-        httpOnly: true, //prevent XSS attack
-        sameSite: 'strict', // CSRF attack cross-site request forgery attack,
-        secure: process.env.NODE_ENV !== 'development',
-        maxAge: 30 * 24 * 60 * 1000,
-    });
-    return token;
+  const token = jsonwebtoken_1.default.sign({ _id }, process.env.SECRET_KEY, {
+    expiresIn: '14d',
+  });
+  res.cookie('access_token', token, {
+    httpOnly: true, //prevent XSS attack
+    sameSite: 'none',
+    secure: process.env.NODE_ENV !== 'development',
+    maxAge: 30 * 24 * 60 * 1000,
+    domain: process.env.HOST_URL,
+  });
+  return token;
 }
 exports.generateToken = generateToken;
